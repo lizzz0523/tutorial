093d893d7d1099fd68d6e855da23a9c04bb8288f
Liz Chow <lizzzled@gmail.com>
2017-07-31 01:40:00 +0800

    add webapp in action

--- chapter-4/example/index.js
+++ chapter-4/example/index.js
@@ -1,9 +1,13 @@
+// 等待页面的dom-ready
 $(init);
 
 function init() {
+  // 获取后台数据
   getData().done(function (data) {
+    // 渲染列表
     $("#list").html(renderList(data.list));
 
+    // 监听列表的点击事件，这里采用了事件代理的方式，减少内存占用
     $("#list").on("click", "li", function () {
       var idx = $(this).data("idx");
       var item = data.list[idx];
@@ -11,7 +15,10 @@ function init() {
       location.href = item.detail_url;
     });
 
+    // 渲染头图
     $("#slider").html(renderSlider(data.imgs));
+    
+    // 初始化swiper插件
     $("#slider").swiper({
       autoplay: 3000,
       speed: 400,
@@ -21,6 +28,7 @@ function init() {
 }
 
 function getData() {
+  // 这里由于还没有实际的后台数据，直接获取本地的json数据作为示意
   return $.getJSON("./data.json");
 }
 
