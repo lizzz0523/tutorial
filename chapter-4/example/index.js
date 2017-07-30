45fc87b0fccf4cac1c19d716e8fee0adc1a29367
Liz Chow <lizzzled@gmail.com>
2017-07-31 01:23:03 +0800

    add slider

--- chapter-4/example/index.js
+++ chapter-4/example/index.js
@@ -3,6 +3,20 @@ $(init);
 function init() {
   getData().done(function (data) {
     $("#list").html(renderList(data.list));
+
+    $("#list").on("click", "li", function () {
+      var idx = $(this).data("idx");
+      var item = data.list[idx];
+
+      location.href = item.detail_url;
+    });
+
+    $("#slider").html(renderSlider(data.imgs));
+    $("#slider").swiper({
+      autoplay: 3000,
+      speed: 400,
+      spaceBetween: 10
+    });
   });
 }
 
@@ -14,9 +28,9 @@ function renderList(list) {
   return list.map(renderItem).join("");
 }
 
-function renderItem(item) {
+function renderItem(item, idx) {
   return `
-    <li>
+    <li data-idx="${ idx }">
       <div class="news">
         <span class="news_cover" style="background-image:url(${ item.cover_url })"></span>
         <div>
@@ -25,5 +39,21 @@ function renderItem(item) {
         </div>
       </div>
     </li>
+  `;
+}
+
+function renderSlider(imgs) {
+  return `
+    <div class="swiper-wrapper">
+        ${ imgs.map(renderSlide).join("") }
+    </div>
+  `;
+}
+
+function renderSlide(img) {
+  return `
+    <div class="swiper-slide">
+      <span class="news-slide" style="background-image:url(${ img })"></span>
+    </div>
   `;
 }
